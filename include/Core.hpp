45932227daaf05783e9eee0424e591b06b68a9e8
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-02-18 10:19:14 +0100

    [ADD] include/Core.hpp

---
+++ include/Core.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2026
+** CORE
+** File description:
+** my_ftp
+*/
+
+#ifndef CORE
+  #define CORE
+  #define SUCCESS 0
+  #define FAIL 1
+  #define ERROR 84
+  #define HELP 2029
+
+#endif /* CORE */
