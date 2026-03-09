543039cd1e3dabfc43ecd529dbab85397439e644
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2026-03-09 16:31:01 +0100

    [UPDATE] main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
+#include <ostream>
 #include "Core.hpp"
+#include "Errors.hpp"
+#include "Server.hpp"
+
+static void printUsage(std::ostream& cstream) {
+  cstream <<
+    "USAGE: ./myftp port path" << std::endl <<
+    " port is the port number on which the server socket listens" << std::endl <<
+    " path is the path to the home directory for the Anonymous user" << std::endl;
+}
 
 static unsigned int handle_args(int argc, const char *argv[]) {
   if (argc != 3 || !argv || !argv[1] || !argv[2]) {
-    std::cerr <<
-      "USAGE: ./myftp port path\n port is the port number on which the server socket listens\n path is the path to the home directory for the Anonymous user" <<
-      std::endl;
+    printUsage(std::cerr);
     return ERROR;
   }
-
   std::string flag(argv[1]);
-  if (flag == "--help" || flag == "-h") {
-    std::cout <<
-      "USAGE: ./myftp port path\n port is the port number on which the server socket listens\n path is the path to the home directory for the Anonymous user" <<
-      std::endl;
+  if (argc == 2 && flag == "--help" || flag == "-h") {
+    printUsage(std::cout);
     return HELP;
   }
   return SUCCESS;
@@ -27,7 +32,13 @@ int main(int argc, const char *argv[]) {
       return ERROR;
     default:
       try {
+        ftp::Server ftpServer(std::stoi(argv[1]), argv[2]);
+
+        ftpServer.run();
         return SUCCESS;
+      } catch (const ftp::FTPError& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return ERROR;
       } catch (...) {
         std::cerr << "Uncaught Error." << std::endl;
         return ERROR;
